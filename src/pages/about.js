c5dd78fee72455c611467204f1c33c9f1f312421
Kumi <ishida.kumiko+git@gmail.com>
2020-07-24 17:11:29 +0700

    Gatsby Tutorial Part 1

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,8 @@ import Header from "../components/header"
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <Header headerText="About Gatsby" />
+      <Header headerText="About Gatsby" arbitraryPhrase="is arbitrary" />
+      <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
     </div>
   )
